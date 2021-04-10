7c61c6632fd6948e648a26299e25072623917015
kevinmoll93 <kevinmollecundo@gmail.com>
2021-04-10 12:35:51 -0300

    Cambios

---
+++ assets/js/carrito.js
@@ -0,0 +1,260 @@
+$(window).on("load", function () {
+
+  let cargarNav = function () {
+    $("#nav").load("assets/templates/nav.html");
+  }
+  cargarNav();
+
+  let cargarPiePag = function(){
+    $("#pie").load("assets/templates/footer.html", function (responseTxt, statusTxt, jqXHR) {
+      if (statusTxt == "success") {
+        positionFooter(-64);
+      }
+    });
+  }
+  cargarPiePag();
+
+
+  let positionFooter = function (margen) {
+    let footerTop = $('#footer').position().top + $('#footer').height();
+    if (footerTop < $(window).height()) {
+      $('#footer').css('margin-top', ((margen) + ($(window).height() - footerTop)) + 'px');
+    }
+  }
+
+  
+
+  let detalleAlCarro = []; //lista de productos
+  let listaProdApi = [];//informacion base para hacer PUT en API
+
+  if (localStorage.carrito) {
+    detalleAlCarro = JSON.parse(localStorage.getItem('carrito'));
+  }
+
+
+  function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+  }
+
+  function removerProd() {
+    $('[name="retirar"]').on("click", function (e) {
+      let identi = $(this).attr("id");
+      removerProd2(identi);
+    });
+  }
+  function removerProd2(idProd) {
+    detalleAlCarro = detalleAlCarro.filter(function (element) {
+
+      if (`${element.data.idTemp}` != idProd) {
+        return true;
+      } else {
+        if (element.data.cantProduct > 1) {
+          element.data.cantProduct--;
+          return true;
+        } else {
+          return false;
+        }
+      }
+    });
+    if(detalleAlCarro.length > 0){
+      localStorage.setItem('carrito', JSON.stringify(detalleAlCarro))
+    }else{
+      localStorage.removeItem('carrito');
+    }
+    cargarTabla();
+  }
+
+  const ordenarArray = function (a, b) {
+    var aName = a.data.name.toLowerCase();
+    var bName = b.data.name.toLowerCase();
+    return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0));
+  }
+
+  const cargarTabla = function () {
+    $('#enviar').hide();
+    listaProdApi = [];
+    $("#table").html("");
+    $("#contTotalSi").hide();
+    $("#contTotalNo").show();
+    $("#total").html("0");
+    let contBodyTable = "";
+    let total = 0;
+    let i = 1;
+    let cantidadMostrar = 1;
+
+    detalleAlCarro.sort(ordenarArray);
+    detalleAlCarro.forEach(element => {
+
+      cantidadMostrar = element.data.cantProduct;
+      // console.log(element.data);
+
+      if (contBodyTable === "") {
+        $('#enviar').show();
+        $("#contTotalSi").show();
+        $("#contTotalNo").hide();
+        contBodyTable += `<thead><tr><th>#</th><th>Descripción</th><th>Imagen</th><th>Val. Unit.</th><th>Cant.</th><th>Total</th><th></th></tr></thead><tbody>`;
+      }
+
+      contBodyTable += `
+      <tr>
+      <td class="">${i}</td>
+      <td class="">${element.data.name}</td>
+      <td class=""><img src="${element.data.imageSrc}" alt="thumb" class="imagenThumb"></td>
+      <td class="numeros ">${formatNumber(element.data.price)}</td>
+      <td class="numeros ">${cantidadMostrar}</td>
+      <td class="numeros ">${formatNumber(parseInt(element.data.price) * parseInt(cantidadMostrar))}</td>
+      <td><button type="button" class="btn btn-light botonEliminar" name="retirar" id="${element.data.name}${element.data.price}${element.data.stock}${element.data.category}${i}" >x</button></td></tr>`;
+
+      let item = {
+        _id: `${element.data.name}${element.data.price}${element.data.stock}${element.data.category}`,
+        name: element.data.name,
+        price: element.data.price,
+        stock: `${cantidadMostrar}`,
+        category: element.data.category,
+        imageSrc: element.data.imageSrc,
+        numProd: `${i}`
+      };
+      listaProdApi.push(item);
+      element.data.idTemp = `${element.data.name}${element.data.price}${element.data.stock}${element.data.category}${i}`;
+      i++;
+      total += parseInt(element.data.price) * parseInt(cantidadMostrar);
+
+    });
+
+    contBodyTable += `</tbody>`;
+    $("#table").html(contBodyTable);
+    $("#total").html(`${formatNumber(total)}`);
+    removerProd();
+    if($('#footer').length > 0){
+      positionFooter(0);
+    }
+  };
+
+  cargarTabla();
+
+
+  $("#enviar").on("click", function () {
+    //verificar stock de todos los productos
+    const getProds = async () => {
+      const Url = 'https://vuvqioudotixkoy.form.io/productos/submission'
+      let listaProds = await axios.get(Url)
+      verificarStock(listaProds);
+    }
+    getProds();
+  });
+
+
+  let verificarStock = function (allProds) {
+    let stockOk = true;
+    let prodEncontrado = false;
+    let todosProdEncontrados = true;
+    for (let prod of listaProdApi) {
+      prodEncontrado = false;
+      allProds.data.forEach(element => {
+        if (prod._id == `${element.data.name}${element.data.price}${element.data.stock}${element.data.category}`) {
+          prodEncontrado = true;
+          if (element.data.stock < prod.stock) {
+            stockOk = false;
+            //mensaje - retirar producto - recargar tabla
+            Swal.fire(`Producto "${element.data.name}" no registra stock suficiente`);
+            removerProd2(`${element.data.name}${element.data.price}${element.data.stock}${element.data.category}${prod.numProd}`)
+          } else {
+            prod._id = element._id;
+            prod.cantBoleta = prod.stock;
+            prod.stock = element.data.stock - prod.stock;
+          }
+        }
+      });
+      if(!prodEncontrado){
+        todosProdEncontrados = false;
+      }
+    }
+    if (todosProdEncontrados && stockOk) {
+      modificarApi(listaProdApi);
+    }
+  }
+
+  let modificarApi = function (listaCompra) {
+    listaCompra.forEach(element => {
+      try {
+        let datosUpdate = {
+          data: {
+            name: element.name,
+            price: element.price,
+            stock: element.stock,
+            category: element.category,
+            imageSrc: element.imageSrc
+          }
+        }
+        let upDateProd = async () => {
+          const apiUrl = 'https://vuvqioudotixkoy.form.io/productos/submission/'
+          let response = await axios.put(`${apiUrl}${element._id}`, datosUpdate)
+        }
+        upDateProd();
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    //todoBien???????????
+    imprimirBoleta(listaCompra);
+  }
+
+  let imprimirBoleta = function (listaCompra) {
+    let pdf = new jsPDF();
+    let fecha = new Date();
+    pdf.text(5, 5, "Boleta Electrónica");
+    pdf.text(5, 12, fecha.getDate() + '/' + (fecha.getMonth() + 1) + '/' + fecha.getFullYear());
+
+    //GENERAR PDF
+    let total = 0;
+    let i = 1;
+    let y = 30;
+    let salto = 10;
+    listaCompra.forEach(element => {
+
+      let maxLength = 40;
+      if (element.name.length > maxLength) {
+        pdf.text(150, y, '  :   $' + formatNumber(parseInt(element.price) * parseInt(element.cantBoleta)));
+        let ArrayNomArt = element.name.split(" ");
+        let firsLine = true;
+        let acumulado = "";
+        for (let w = 0; w < ArrayNomArt.length; w++) {
+
+          if ((acumulado + " " + ArrayNomArt[w]).length > maxLength) {
+            if (firsLine) {
+              pdf.text(10, y, i + '.- ' + ' ' + formatNumber(element.cantBoleta) + '  *  ' + acumulado);
+              firsLine = false;
+            } else {
+              pdf.text(30, y, acumulado);
+            }
+
+            acumulado = ArrayNomArt[w];
+            y = y + salto;
+          } else {
+            acumulado += " " + ArrayNomArt[w];
+          }
+
+        }
+        if (acumulado != '') {
+          pdf.text(30, y, acumulado);
+          acumulado = "";
+          y = y + salto;
+        }
+
+      } else {
+        pdf.text(10, y, i + '.- ' + ' ' + formatNumber(element.cantBoleta) + '  *  ' + element.name);
+        pdf.text(150, y, '  :   $' + formatNumber(parseInt(element.price) * parseInt(element.cantBoleta)));
+        y = y + salto;
+      }
+
+      i++;
+      total += parseInt(element.price) * parseInt(element.cantBoleta);
+    });
+    pdf.text(130, (y + 15), 'Total     ' + '  : $' + formatNumber(total));
+    pdf.save("sample-file.pdf");
+    localStorage.removeItem("carrito");
+    $('#ModalRespuesta').modal('show');
+  }
+  // positionFooter();
+
+});
---
+++ assets/js/index.js
@@ -0,0 +1,314 @@
+$(document).ready(function () {
+  ShowTotal()
+  // Llamado a la lista de productos
+  getProducts()
+    .then(res => {
+      res.data ? renderProductList(res.data) : alert()
+    })
+    .catch(e => console.log(e))
+})
+
+// Funciones Lógicas para la lista de productos
+const getProducts = async page => {
+  let apiUrl = 'https://vuvqioudotixkoy.form.io/productos/submission?limit=4'
+  if(page){
+      // console.log(page)
+      apiUrl = `${apiUrl}&skip=${page}`
+      $('.anterior').css('display', 'block')
+  }
+    else {
+    $('.anterior').css('display', 'none')
+  } 
+  let response = await axios.get(apiUrl)
+  return response
+}
+
+const renderProductList = productArray => {
+  $('.product-list').html('')
+  let arrayLength = productArray.length
+  let pages = Math.ceil(arrayLength / 4)
+
+  if (arrayLength <= 0) {
+    $('.product-list').append(`
+    <div class>
+      <h1>Error</h1>
+      <p>Su búsqueda no arrojó datos :(</p>
+    </div>
+  `)
+  } else if (arrayLength <= 4) {
+    productArray.forEach((entry, i) => {
+      let product = entry.data
+      let { name, price, stock, category, imageSrc } = product
+
+      $('.product-list').append(createProductTemplate(name, price, stock, category, imageSrc, i))
+    })
+  } else {
+    for (let i = 0; i <= 4; i++) {
+      let product = productArray[i].data
+      let { name, price, stock, category, imageSrc } = product
+      $('.product-list').append(createProductTemplate(name, price, stock, category, imageSrc, i))
+    }
+    $('.pagination').append(createPaginationTemplate(pages))
+  }
+}
+
+const createProductTemplate = (name, price, stock, category, imageSrc, i) => {
+  let template = `
+  
+  <div class="card col-3 my-2 py-3">
+    <h6 class="card-title titleOne"> <label class ="card-title">${name}</label> </h6>
+      <div id="imagenesCards" style="background-image: url(${imageSrc})" ></div>  
+      <p class="card-text"> Valor: $ ${price}</p>      
+      <p class="product__stock card-text"><strong>Stock:</strong> ${stock}</p>
+      <p class="card-text category"><strong>Categoría:</strong>  ${i} <a href="#">${category}</a></p>
+
+
+    <p class="card-text"><strong>Cantidad:</strong> <input type="number" class="id-${i} inputCAntidad" value="1" id="cantProducts">
+
+    <button class="btn btn-success botonAgregar" onclick="addProduct('${name}','${imageSrc}','${price}','${stock}','${category}', $('.id-${i}').val())">Agregar al carro</button>
+
+  </div>
+  
+`
+  return template
+}
+
+const createPaginationTemplate = pages => {
+  let prevButton = `<li class="page-item"><a class="page-link" href="#">Previous</a></li>`
+  let nextButton = `<li class="page-item"><a class="page-link" href="#">Next</a></li>`
+  let pageButtons = ''
+  for (let i = 1; i <= pages; i++) {
+    pageButtons += `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`
+  }
+  let template = prevButton + pageButtons + nextButton
+  return template
+}
+
+//  Fin de las funciones para la lista de productos
+
+// Lógica para la busqueda general
+//falta entonces que navegue entre las paginas
+ 
+$('#search').keypress(function(event){
+  let keycode = (event.keyCode ? event.keyCode : event.which);
+  if(keycode == '13'){
+    $('.product-list').html('');
+    $.ajax({
+           url: `https://vuvqioudotixkoy.form.io/productos/submission`,
+           success: function(data){
+            for (i in data){
+              $('.product-list').append(`
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title titleOne" id="aqui"><label class="card-title">${data[i].data.name} </label></h5>
+                <div id="cuerpo">
+                  <img src="${data[i].data.imageSrc}" class="card-img-top" alt="...">
+                  <p class="card-text"><label id = "price" value="${data[i].data.price}"> Valor: $${data[i].data.price}</label></p>
+                  <p class="card-text"> Stock: ${data[i].data.stock}</p>
+                 <p class="card-text"> Categoria: ${data[i].data.category}</p>
+
+                 <p class="card-text"><strong>Cantidad:</strong> <input type="number" class="id-${i}" value="1" id="cantProducts">
+
+
+                <button class="btn btn-success" onclick="addProduct('${data[i].data.name}','${data[i].data.imageSrc}','${data[i].data.price}','${data[i].data.stock}',' ${data[i].data.category}', $('.id-${i}').val())">Agregar al carro</button>
+                </div>
+              </div>
+          </div>
+            `)
+            }
+       }
+       
+      
+  }).done(
+    function() {
+      let producto = $('.card-title');
+      let buscando = document.getElementById('search').value
+      let item='';
+      for( let i = 0; i < producto.length; i++ ){
+        item = $(producto[i]).html().toLowerCase();
+          for(let x = 0; x < item.length; x++ ){
+           if( buscando.length == 0 || item.indexOf( buscando ) > -1 ){
+               $(producto[i]).parents('.card-body').show(); 
+           }else{
+                $(producto[i]).parents('.card-body').hide();
+           }
+       }
+      } 
+     }
+   )
+  }
+})
+
+
+$('.btnCategory').click(function() {
+  let valor = this.value
+  console.log(valor);
+  $('.product-list').html('');
+    $.ajax({
+           url: `https://vuvqioudotixkoy.form.io/productos/submission`,
+           success: function(data){
+            for (i in data){
+              $('.product-list').append(`
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title titleOne" id="aqui"><label class="card-title">${data[i].data.name} </label></h5>
+                <div id="cuerpo">
+                  <img src="${data[i].data.imageSrc}" class="card-img-top" alt="...">
+                  <p class="card-text"><label id = "price" value="${data[i].data.price}"> Valor: $${data[i].data.price}</label></p>
+                  <p class="card-text"> Stock: ${data[i].data.stock}</p>
+                 <p class="card-text category"> Categoria: ${data[i].data.category}</p>
+
+                 <p class="card-text"><strong>Cantidad:</strong> <input type="number" class="id-${i}" value="1" id="cantProducts">
+
+                <button class="btn btn-success" onclick="addProduct('${data[i].data.name}','${data[i].data.imageSrc}','${data[i].data.price}','${data[i].data.stock}',' ${data[i].data.category}', $('.id-${i}').val())">Agregar al carro</button>
+                </div>
+              </div>
+          </div>
+            `)
+            }
+       }  
+  }).done(function(){
+    
+    let producto = $('.category');
+    for( let i = 0; i < producto.length; i++ ){
+    item = $(producto[i]).html().toLowerCase();
+     for(let x = 0; x < item.length; x++ ){
+         if( valor.length == 0 || item.indexOf( valor ) > -1 ){
+             $(producto[i]).parents('.card-body').show(); 
+         }else{
+              $(producto[i]).parents('.card-body').hide();
+         }
+     }
+    }
+  })
+});
+
+
+$('#search').keyup(function () {
+  let productoTitle = $('.card-title');
+  let buscando = $(this).val();
+  let item = '';
+  for (let i = 0; i < productoTitle.length; i++) {
+    item = $(productoTitle[i]).html().toLowerCase();
+    for (let x = 0; x < item.length; x++) {
+      if (buscando.length == 0 || item.indexOf(buscando) > -1) {
+        $(productoTitle[i]).parents('.card-title').show();
+      } else {
+        $(productoTitle[i]).parents('.card-title').hide();
+      }
+    }
+  }
+})
+
+// Fin de la logica para la busqueda general
+
+//  Lógica para agregar productos al carrito
+
+function addProduct(name, imageSrc, price, stock, category, cantProduct) {
+  if (cantProduct > 0) {
+    let detalleAlCarro = []
+    if (!localStorage.getItem('carrito')) {
+      detalleAlCarro.push({
+        data: { name, price, stock, imageSrc, category, cantProduct: cantProduct },
+      })
+
+      localStorage.setItem('carrito', JSON.stringify(detalleAlCarro))
+      alert('Producto Guardado en el carrito')
+    } else {
+      detalleAlCarro = JSON.parse(localStorage.getItem('carrito'))
+      verificarProdYaSelected(name, imageSrc, price, stock, category, cantProduct, detalleAlCarro);
+    }
+  } else {
+    alert('Ingrese una cantidad lógica')
+  }
+  ShowTotal();
+}
+
+function verificarProdYaSelected(name, imageSrc, price, stock, category, cantProduct, detalleAlCarro) {
+  let prodYaExiste = false;
+  detalleAlCarro.forEach(element => {
+    if ((name + imageSrc + price + stock + category) == (element.data.name + element.data.imageSrc + element.data.price + element.data.stock + element.data.category)) {
+      prodYaExiste = true;
+      Swal.fire({
+        showClass: {
+          popup: 'animated fadeInDown faster'
+        },
+        hideClass: {
+          popup: 'animated fadeOutUp faster'
+        },
+        title: 'Aviso, contenido del carrito',
+        text: `El producto seleccionado "${name}" ya tiene ${element.data.cantProduct} unidades en el carrito. ¿Quiere agregar ${cantProduct} más?`,
+        icon: 'warning',
+        showCancelButton: true,
+        cancelButtonText: 'Cancelar',
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Agregar'
+      }).then((result) => {
+        if (result.value) {
+          element.data.cantProduct = parseInt(element.data.cantProduct) + parseInt(cantProduct);
+          localStorage.setItem('carrito', JSON.stringify(detalleAlCarro))
+          alert('Producto Guardado en el carrito')
+          ShowTotal();
+        }
+      })
+    }
+  });
+  if (!prodYaExiste) {
+    detalleAlCarro.push({
+      data: { name, price, stock, imageSrc, category, cantProduct: cantProduct }
+    })
+    localStorage.setItem('carrito', JSON.stringify(detalleAlCarro))
+    alert('Producto Guardado en el carrito')
+    ShowTotal();
+  }
+}
+
+let page = 0
+
+function despuesPage() {
+  page += 4
+  
+  getProducts(page)
+  
+    .then(res => {
+      renderProductList(res.data)
+
+      if (res.data[res.data.length - 1].data.send) {
+        $('.despues').css('display', 'none')
+      }
+    })
+    .catch(e => {
+      page -= 4
+      $('.despues').css('display', 'none')
+    })
+}
+
+function atrasPage() {
+  if (page <= 0) {
+  } else {
+    page -= 4
+    getProducts(page)
+      .then(res => {
+        renderProductList(res.data)
+        $('.despues').css('display', 'block')
+      })
+      .catch(e => {
+        $('.anterior').css('display', 'none')
+      })
+  }
+}
+
+const ShowTotal = () => {
+  if (localStorage.carrito) {
+    let localsito = JSON.parse(localStorage.getItem('carrito'))
+
+    let total = 0
+    localsito.forEach(p => {
+      total += parseInt(p.data.price) * parseInt(p.data.cantProduct)
+    })
+
+    $('#total').html(total.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.'))
+  }
+}
+
